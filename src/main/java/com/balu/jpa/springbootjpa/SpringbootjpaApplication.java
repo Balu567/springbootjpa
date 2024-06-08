2ef464a41bac9e01a7d6dcf4e951f3c7732c1892
Balu567 <balarajumalla.9@gmail.com>
2024-06-09 00:48:20 +1000

    Local cleanup

--- src/main/java/com/balu/jpa/springbootjpa/SpringbootjpaApplication.java
+++ src/main/java/com/balu/jpa/springbootjpa/SpringbootjpaApplication.java
@@ -3,7 +3,10 @@ package com.balu.jpa.springbootjpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication(scanBasePackages = {"com.balu.jpa.springbootjpa"})
+@EnableSwagger2
 public class SpringbootjpaApplication {
 
 	public static void main(String[] args) {
